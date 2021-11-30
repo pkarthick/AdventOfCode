4cc2c5b78d33e8cd30c72483a2ad7051a3250d67
Karthick Pachiappan <pkarthick@outlook.com>
2021-11-30 23:02:40 +0530

    Getting ready for AoC 2021. This time using Rust.

---
+++ 2021/rust/src/day1/part2.rs
@@ -0,0 +1,31 @@
+use core::*;
+
+struct PartTwo {}
+
+impl PartSpec for PartTwo {
+    fn get_day(&self) -> i32 {
+        1
+    }
+
+    fn get_part_kind(&self) -> PartKind {
+        PartKind::Two
+    }
+}
+
+impl TestPart for PartTwo {
+    fn process_input(&self, input: String) -> String {
+        input.to_string()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn test() {
+        let part = PartTwo{};
+        assert!(part.test_sample().is_ok());
+        assert!(part.test_puzzle().is_ok());
+    }
+}
