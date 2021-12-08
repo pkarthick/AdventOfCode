c6b35600e9642a90633950b4b9d62ab1c1c2a3c5
Karthick Pachiappan <pkarthick@outlook.com>
2021-12-08 10:45:57 +0530

    Day 5 using Crystal

---
+++ 2021/crystal/day05_hypothermal_venture/part1.cr
@@ -0,0 +1,54 @@
+require "../day.cr"
+
+struct Line
+  def initialize(@start : Tuple(Int32, Int32), @finish : Tuple(Int32, Int32))
+  end
+
+  def get_delta(x : Int32, y : Int32)
+    if x == y
+      0
+    elsif x < y
+      1
+    else
+      -1
+    end
+  end
+
+  def points
+    sx, sy = @start
+    fx, fy = @finish
+
+    xd = get_delta(sx, fx)
+    yd = get_delta(sy, fy)
+
+    pts = [] of Tuple(Int32, Int32)
+
+    if sx == fx || sy == fy
+      while sx != fx || sy != fy
+        pts << {sx, sy}
+        sx += xd
+        sy += yd
+      end
+      pts << {fx, fy}
+    end
+
+    pts
+  end
+
+  def Line.from(l : String)
+    pts = l.split(" -> ").map(&.split(',').map(&.to_i))
+    sx, sy = pts[0][0].to_i, pts[0][1].to_i
+    fx, fy = pts[1][0].to_i, pts[1][1].to_i
+    Line.new({sx, sy}, {fx, fy})
+  end
+end
+
+puts Day.new(5, 1).execute { |input|
+  input
+    .lines
+    .flat_map { |l| (Line.from l).points }
+    .group_by { |x| x }
+    .map { |_, v| v.size }
+    .count { |c| c > 1 }
+    .to_s
+}
---
+++ 2021/crystal/day05_hypothermal_venture/part2.cr
@@ -0,0 +1,52 @@
+require "../day.cr"
+
+struct Line
+  def initialize(@start : Tuple(Int32, Int32), @finish : Tuple(Int32, Int32))
+  end
+
+  def get_delta(x : Int32, y : Int32)
+    if x == y
+      0
+    elsif x < y
+      1
+    else
+      -1
+    end
+  end
+
+  def points
+    sx, sy = @start
+    fx, fy = @finish
+
+    xd = get_delta(sx, fx)
+    yd = get_delta(sy, fy)
+
+    pts = [] of Tuple(Int32, Int32)
+
+    while sx != fx || sy != fy
+      pts << {sx, sy}
+      sx += xd
+      sy += yd
+    end
+    pts << {fx, fy}
+
+    pts
+  end
+
+  def Line.from(l : String)
+    pts = l.split(" -> ").map(&.split(',').map(&.to_i))
+    sx, sy = pts[0][0].to_i, pts[0][1].to_i
+    fx, fy = pts[1][0].to_i, pts[1][1].to_i
+    Line.new({sx, sy}, {fx, fy})
+  end
+end
+
+puts Day.new(5, 2).execute { |input|
+  input
+    .lines
+    .flat_map { |l| (Line.from l).points }
+    .group_by { |x| x }
+    .map { |_, v| v.size }
+    .count { |c| c > 1 }
+    .to_s
+}
