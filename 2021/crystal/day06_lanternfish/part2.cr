4a756b88c04d779d7ead2a53642bd91857b08edf
Karthick Pachiappan <pkarthick@outlook.com>
2021-12-08 21:48:48 +0530

    Day 6 using Crystal

---
+++ 2021/crystal/day06_lanternfish/part2.cr
@@ -0,0 +1,27 @@
+require "../day.cr"
+
+puts Day.new(6, 2).execute { |input|
+  age_groups = Hash(Int32, Int64).new(0)
+
+  input
+    .split(',')
+    .map(&.to_i)
+    .each do |age|
+      age_groups[age] += 1
+    end
+
+  (1..256).each do |_|
+    new_count = age_groups[0]
+
+    (1..8).each do |age|
+      age_groups[age - 1] = age_groups[age]
+    end
+
+    age_groups[8] = new_count
+    age_groups[6] += new_count
+  end
+
+  puts age_groups.values.sum.to_s
+
+  age_groups.values.sum.to_s
+}
