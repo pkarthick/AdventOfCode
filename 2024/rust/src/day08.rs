20f29ecf27fbea5358d35b6fd3f2887692be79c2
Karthick Pachiappan <pkarthick@outlook.com>
2024-12-10 20:45:02 +0530

    Day 8 and Day 9 using Rust

--- 2024/rust/src/day08.rs
+++ 2024/rust/src/day08.rs
@@ -1,15 +1,44 @@
-use std::str::FromStr;
+use std::{
+    collections::{HashMap, HashSet}, str::FromStr
+};
 
 use crate::day::AoCDay;
 
-pub struct Day {}
+pub struct Day {
+    antennas: HashMap<char, Vec<(i8, i8)>>,
+    row_count: usize,
+    col_count: usize,
+    antenna_locations: HashSet<(i8, i8)>,
+}
 
 impl FromStr for Day {
     type Err = ();
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let _ = s;
-        Ok(Day {})
+        let mut antennas = HashMap::new();
+        let mut antenna_locations: HashSet<(i8, i8)> = HashSet::new();
+
+        let lines: Vec<&str> = s.split('\n').collect();
+        let row_count = lines.len();
+        let col_count = lines[0].len();
+
+        for (r, line) in lines.into_iter().enumerate() {
+            for (c, ch) in line.chars().enumerate().filter(|(_, ch)| *ch != '.') {
+                let rc = (r as i8, c as i8);
+                antennas
+                    .entry(ch)
+                    .and_modify(|v: &mut Vec<(i8, i8)>| v.push(rc))
+                    .or_insert(vec![rc]);
+                antenna_locations.insert(rc);
+            }
+        }
+
+        Ok(Day {
+            antennas,
+            row_count,
+            col_count,
+            antenna_locations,
+        })
     }
 }
 
@@ -17,17 +46,65 @@ impl Day {
     pub fn new(input: &str) -> Self {
         Day::from_str(input).unwrap()
     }
+
+    fn combinations(locs: &Vec<(i8, i8)>) -> Vec<((i8, i8), (i8, i8))> {
+        let mut combs = vec![];
+
+        for i in 0..locs.len() {
+            for j in i + 1..locs.len() {
+                combs.push((locs[i], locs[j]));
+            }
+        }
+
+        combs
+    }
+
+    fn get_antinode_locations(&self, part1: bool) -> HashSet<(i8, i8)> {
+        let mut uniq: HashSet<(i8, i8)> = HashSet::new();
+
+        for (_a, locs) in self.antennas.iter() {
+            for ((r1, c1), (r2, c2)) in Day::combinations(locs) {
+
+                let rd = r1 - r2;
+                let cd = c1 - c2;
+
+                self.insert_antinodes(r1, c1, rd, cd, &mut uniq, part1);
+                self.insert_antinodes(r2, c2, -rd, -cd, &mut uniq, part1);
+
+            }
+        }
+
+        return uniq;
+    }
+
+    fn insert_antinodes(&self, r: i8, c: i8, rd: i8, cd: i8, uniq: &mut HashSet<(i8, i8)>, part1: bool) {
+        let mut rr = r;
+        let mut cc = c;
+
+        loop {
+            rr += rd;
+            cc += cd;
+
+            if 0 <= rr && rr < self.row_count as i8 && 0 <= cc && cc < self.col_count as i8 {
+                uniq.insert((rr, cc));
+            } else {
+                break;
+            }
+
+            if part1 {
+                break;
+            }
+        }
+    }
 }
 
 impl AoCDay for Day {
     fn part1(&mut self) -> String {
-        println!("Part 1 not implemented");
-        "".to_string()
+        self.get_antinode_locations(true).len().to_string()
     }
 
     fn part2(&mut self) -> String {
-        println!("Part 2 not implemented");
-        "".to_string()
+        self.get_antinode_locations(false).union(&self.antenna_locations).count().to_string()
     }
 }
 
@@ -38,13 +115,13 @@ mod tests {
     #[test]
     fn test_part1_input() {
         let actual = Day::new(INPUT).part1();
-        assert_eq!(actual, "");
+        assert_eq!(actual, "14");
     }
 
     #[test]
     fn test_part2_input() {
         let actual = Day::new(INPUT).part2();
-        assert_eq!(actual, "");
+        assert_eq!(actual, "34");
     }
 
     #[test]
@@ -65,6 +142,16 @@ mod tests {
 
     const PUZZLE_INPUT: &str = include_str!("../data/day08");
 
-    const INPUT: &str = "";
+    const INPUT: &str = "............
+........0...
+.....0......
+.......0....
+....0.......
+......A.....
+............
+............
+........A...
+.........A..
+............
+............";
 }
-
--- 2024/rust/src/day09.rs
+++ 2024/rust/src/day09.rs
@@ -2,14 +2,51 @@ use std::str::FromStr;
 
 use crate::day::AoCDay;
 
-pub struct Day {}
+struct Block {
+    free: bool,
+    num: Option<usize>,
+    size: usize,
+}
+
+pub struct Day {
+    blocks: Vec<Block>,
+    data: Vec<Option<usize>>,
+}
 
 impl FromStr for Day {
     type Err = ();
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let _ = s;
-        Ok(Day {})
+        let mut blocks = vec![];
+        let mut data = vec![];
+
+        for size in s.chars().map(|c| c.to_digit(10).unwrap() as usize) {
+            if blocks.len() % 2 == 0 {
+                blocks.push(Block {
+                    free: false,
+                    num: Some(blocks.len() / 2),
+                    size,
+                });
+                data.extend(
+                    std::iter::repeat(Some(blocks.len() / 2))
+                        .take(size as usize)
+                        .collect::<Vec<Option<usize>>>(),
+                );
+            } else {
+                blocks.push(Block {
+                    free: true,
+                    num: None,
+                    size,
+                });
+                data.extend(
+                    std::iter::repeat(None)
+                        .take(size as usize)
+                        .collect::<Vec<Option<usize>>>(),
+                );
+            }
+        }
+
+        Ok(Day { blocks, data })
     }
 }
 
@@ -17,20 +54,113 @@ impl Day {
     pub fn new(input: &str) -> Self {
         Day::from_str(input).unwrap()
     }
+
+    fn defrag(&mut self) {
+        let mut end = self.blocks.len() - 1;
+
+        loop {
+            while end > 0 {
+                if self.blocks[end].free {
+                    if end == 0 {
+                        break;
+                    }
+                    end -= 1;
+                } else {
+                    break;
+                }
+            }
+
+            if end == 0 {
+                break;
+            }
+
+            let mut changed = false;
+
+            for start in 0..end + 1 {
+                // let mut self.blocks[start] = &mut self.blocks[start];
+                // let mut end_block = &mut self.blocks[end];
+
+                if self.blocks[start].free {
+                    if self.blocks[start].size == self.blocks[end].size {
+                        changed = true;
+                        self.blocks[start].num = self.blocks[end].num;
+                        self.blocks[end].num = None;
+                        self.blocks[start].free = false;
+                        self.blocks[end].free = true;
+                        break;
+                    } else if self.blocks[start].size > self.blocks[end].size {
+                        changed = true;
+                        let start_size = self.blocks[start].size;
+                        self.blocks[start].num = self.blocks[end].num;
+                        self.blocks[end].num = None;
+                        self.blocks[start].free = false;
+                        self.blocks[end].free = true;
+                        self.blocks[start].size = self.blocks[end].size;
+                        self.blocks.insert(
+                            start + 1,
+                            Block {
+                                free: true,
+                                num: None,
+                                size: start_size - self.blocks[end].size,
+                            },
+                        );
+                        break;
+                    }
+                }
+            }
+            if !changed {
+                end -= 1
+            }
+        }
+    }
 }
 
 impl AoCDay for Day {
     fn part1(&mut self) -> String {
-        println!("Part 1 not implemented");
-        "".to_string()
+        let mut start = 0;
+        let mut end = self.data.len() - 1;
+        let mut total: usize = 0;
+
+        while start <= end {
+            while start <= end && self.data[start].is_some() {
+                total += self.data[start].unwrap() as usize * start;
+                start += 1;
+            }
+
+            while start <= end && self.data[start].is_none() {
+                while self.data[end].is_none() {
+                    end -= 1;
+                }
+
+                total += self.data[end].unwrap() as usize * start;
+                start += 1;
+                end -= 1;
+            }
+        }
+
+        total.to_string()
     }
 
     fn part2(&mut self) -> String {
-        println!("Part 2 not implemented");
-        "".to_string()
+        self.defrag();
+
+        let mut i = 0;
+        let mut total = 0;
+
+        for block in self.blocks.iter() {
+            if !block.free {
+                for _ in 0..block.size {
+                    total += i * block.num.unwrap();
+                    i += 1;
+                }
+            } else {
+                i += block.size;
+            }
+        }
+
+        return total.to_string();
     }
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -39,13 +169,13 @@ mod tests {
     #[test]
     fn test_part1_input() {
         let actual = Day::new(INPUT).part1();
-        assert_eq!(actual, "");
+        assert_eq!(actual, "1928");
     }
 
     #[test]
     fn test_part2_input() {
         let actual = Day::new(INPUT).part2();
-        assert_eq!(actual, "");
+        assert_eq!(actual, "2858");
     }
 
     #[test]
@@ -66,5 +196,5 @@ mod tests {
 
     const PUZZLE_INPUT: &str = include_str!("../data/day09");
 
-    const INPUT: &str = "";
+    const INPUT: &str = "2333133121414131402";
 }
